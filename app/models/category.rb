57aa9581b0bcc6c285772f03fb0a5484378addf5
Reed S. MacConnell <learn22ruby@gmail.com>
2013-09-12 11:39:20 -0400

    changed categories_posts to category_post

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :category_posts #might not be needed
   has_many :posts, through: :category_posts
+
+#Alternatives associations I therorize might fix my issues  
+  #belongs_to :category_posts
+  #belongs_to :posts, through: category_posts
 end
 
 #unsure of pluralization
